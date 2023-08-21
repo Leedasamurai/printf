4256b7315168c59a9d4e0bb91add2fd29ee99db8
Leedasamurai <lesegoleemac@gmail.com>
2023-08-20 19:57:56 -0700

    main.h

--- main.h
+++ main.h
@@ -5,8 +5,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
-#ifndef MAIN_H
-#define MAIN_H
 
 int _putchar(char c);
 void _puts(const char *str);
@@ -15,12 +13,17 @@ void print_char(va_list args);
 void print_str(va_list args);
 void print_perc(va_list args);
 
-typedef struct
+/**
+ * struct hs - A structure for handling format spec and corresponding func.
+ * @sp: The format specifier character.
+ * @h: Pointer to the corresponding function for the format specifier.
+ */
+typedef struct hs
 {
-	char sp; /* The specifier character */
-	void (*h)(va_list); /* Pointer to the handler function */
+	char sp;
+	void (*h)(va_list);
 } hs;
 
 extern hs sph[];
 
-#endif
+#endif /* MAIN_H */
