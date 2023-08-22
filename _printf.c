9b2c0e62d9e2f2d9dfed73955bdeae4e17d1b9e3
Leedasamurai <lesegoleemac@gmail.com>
2023-08-22 14:50:14 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,131 +1,66 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * print_char - Prints a single character.
- * @args: The va_list containing the variable argument list.
- */
-void print_char(va_list args)
-{
-	int c = va_arg(args, int);
-
-	_putchar(c);
-}
-
-/**
- * print_str - Prints a string of characters.
- * @args: The va_list containing the variable argument list.
- */
-void print_str(va_list args)
-{
-	char *str = va_arg(args, char *);
-
-	if (str == NULL)
-	{
-		_putchar('(');
-		_putchar('n');
-		_putchar('u');
-		_putchar('l');
-		_putchar('l');
-		_putchar(')');
-
-		return;
-	}
-
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-}
-
-/**
- * print_perc - Prints the '%' character.
- * @args: The va_list containing the variable argument list.
- */
-void print_perc(va_list args)
-{
-	(void)args;
-	_putchar('%');
-}
-
-/**
- * print_unknown - Prints the 'Unknown' placeholder.
- * @args: The va_list containing the variable argument list.
- */
-void print_unknown(va_list args)
-{
-	(void)args;
-	_putchar('[');
-	_putchar('%');
-	_putchar('r');
-	_putchar(']');
-}
-
-/**
- * _printf - Custom printf function that handles specified format specifiers.
- * @format: The format string containing characters and format specifiers.
- * @...: Additional arguments corresponding to the format specifiers.
- *
- * Return: The number of characters printed (excluding the null byte).
+ * _printf - printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	int i;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	va_start(args, format);
+	if (format == NULL)
+		return (-1);
 
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
-			_putchar(format[i]);
-			count++;
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
-
 		else
 		{
-			i++;
-			if (format[i] == ' ')
-			{
-				va_end(args);
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+			if (printed == -1)
 				return (-1);
-			}
-
-			if (format[i] == 'c')
-			{
-				int c = va_arg(args, int);
-
-				_putchar(c);
-				count++;
-			}
-
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-				int len = (str == NULL) ? fputs("(null)", stdout) : fputs(str, stdout);
-
-				count += (len == -1) ? 6 : len;
-			}
-
-			else if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-
-			else if (format[i] == 'r')
-			{
-				print_unknown(args);
-				count += 4;
-			}
+			printed_chars += printed;
 		}
 	}
-	va_end(args);
-	return (count);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
